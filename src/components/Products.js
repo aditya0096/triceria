439b9b656b697318454cbfc4bbe5c5162429b015
aditya0096 <adityajadhav0096@gmail.com>
2021-04-21 10:33:37 +0530

    commit

--- src/components/Products.js
+++ src/components/Products.js
@@ -7,17 +7,15 @@ export default class Products extends Component {
                 <ul className="products">
                     {this.props.products.map(product => (
                         <li key={product.id}>
-                            <div className="product">
-                            <a href={"#"+ product.id}>
+                        <div className="product">
+                            <a href={"#"+ product.id}></a>
                                 <img src={product.image} alt={product.title}></img>
                                 <p><h1>{product.title}</h1></p>
-                            </a>
                             <div className="product-price">
                                 <div>{product.price}</div>
-                                <button>{product.quantity}</button>
                                 <button className="button primary">Add to Cart</button>
                             </div>
-                            </div>
+                        </div>
                         </li>
                     ))}
                 </ul>
